0187182cddebb18add6246781feab441b1c13def
aliffauzi34 <alifm5834@gmail.com>
2025-10-15 02:37:53 +0700

    feat(php): move hello-world project to 2025/php directory

--- php/hello-world/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Simple Hello World app for Hacktoberfest
-date_default_timezone_set("Asia/Jakarta");
-echo "<h1>Hello, Hacktoberfest!</h1>";
-echo "<p>PHP says hi at " . date('Y-m-d H:i:s') . "</p>";
-?>
